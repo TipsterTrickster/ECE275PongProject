d62d920ddbabd388f8e129880bd70bece3bb4d50
TipsterTrickster <tipstertrickster13@gmail.com>
2023-11-20 16:51:12 -0500

    something is moving

--- Project.sv
+++ Project.sv
@@ -1,36 +1,43 @@
-`include "./DE0_VGA.v"
+`include "./DE0_VGA.sv"
 `include "./modules/make_box.sv"
 
 module Project(CLOCK_50, 
                 VGA_R, VGA_G, VGA_B, VGA_HS, VGA_VS);
 
-input	wire			CLOCK_50;
+input	logic			CLOCK_50;
 
-output	wire	[3:0]		VGA_R;		//Output Red
-output	wire	[3:0]		VGA_G;		//Output Green
-output	wire	[3:0]		VGA_B;		//Output Blue
+output	logic	[3:0]		VGA_R;		//Output Red
+output	logic	[3:0]		VGA_G;		//Output Green
+output	logic	[3:0]		VGA_B;		//Output Blue
 
-output	wire	[0:0]		VGA_HS;			//Horizontal Sync
-output	wire	[0:0]		VGA_VS;			//Vertical Sync
+output	logic	[0:0]		VGA_HS;			//Horizontal Sync
+output	logic	[0:0]		VGA_VS;			//Vertical Sync
 
-wire			[9:0]		X_pix;			//Location in X of the driver
-wire			[9:0]		Y_pix;			//Location in Y of the driver
+logic			[9:0]		X_pix;			//Location in X of the driver
+logic			[9:0]		Y_pix;			//Location in Y of the driver
 
-wire			[0:0]		H_visible;		//H_blank?
-wire			[0:0]		V_visible;		//V_blank?
+logic			[0:0]		H_visible;		//H_blank?
+logic			[0:0]		V_visible;		//V_blank?
 
-wire		[0:0]		pixel_clk;		//Pixel clock. Every clock a pixel is being drawn. 
-wire			[9:0]		pixel_cnt;		//How many pixels have been output.
+logic		[0:0]		pixel_clk;		//Pixel clock. Every clock a pixel is being drawn. 
+logic			[9:0]		pixel_cnt;		//How many pixels have been output.
 
-reg			[11:0]		pixel_color;	//12 Bits representing color of pixel, 4 bits for R, G, and B
+logic			[11:0]		pixel_color;	//12 Bits representing color of pixel, 4 bits for R, G, and B
 										//4 bits for Blue are in most significant position, Red in least
 
 // Draw the player one paddle
 wire [9:0] player_1_paddle_width = 10;
 wire [9:0] player_1_paddle_height = 50;
-wire [9:0] player_1_paddle_x_location = 0;
-wire [9:0] player_1_paddle_y_location = 220;
-reg player_1_paddle;
+logic [9:0] player_1_paddle_x_location_logic = 0;
+logic [9:0] player_1_paddle_y_location_logic = 220;
+
+wire [9:0] player_1_paddle_x_location;
+wire [9:0] player_1_paddle_y_location;
+
+logic player_1_paddle;
+
+assign player_1_paddle_x_location = player_1_paddle_x_location_logic;
+assign player_1_paddle_y_location = player_1_paddle_y_location_logic;
 
 make_box draw_player_1_paddle(
 	.X_pix(X_pix),
@@ -45,9 +52,17 @@ make_box draw_player_1_paddle(
 
 wire [9:0] player_2_paddle_width = 10;
 wire [9:0] player_2_paddle_height = 50;
-wire [9:0] player_2_paddle_x_location = 630;
-wire [9:0] player_2_paddle_y_location = 220;
-reg player_2_paddle;
+logic [9:0] player_2_paddle_x_location_logic = 0;
+logic [9:0] player_2_paddle_y_location_logic = 220;
+
+wire [9:0] player_2_paddle_x_location;
+wire [9:0] player_2_paddle_y_location;
+
+logic player_2_paddle;
+
+assign player_2_paddle_x_location = player_2_paddle_x_location_logic;
+assign player_2_paddle_y_location = player_2_paddle_y_location_logic;
+
 
 make_box draw_player_2_paddle(
 	.X_pix(X_pix),
@@ -60,11 +75,19 @@ make_box draw_player_2_paddle(
 	.box(player_2_paddle)
 );
 
-wire [9:0] ball_width = 7;
-wire [9:0] ball_height = 7;
-wire [9:0] ball_x_location = 300;
-wire [9:0] ball_y_location = 220;
-reg ball;
+wire [9:0] ball_width = 10;
+wire [9:0] ball_height = 50;
+logic [9:0] ball_x_location_logic = 0;
+logic [9:0] ball_y_location_logic = 220;
+
+wire [9:0] ball_x_location;
+wire [9:0] ball_y_location;
+
+logic ball;
+
+assign ball_x_location = ball_x_location_logic;
+assign ball_y_location = ball_y_location_logic;
+
 
 make_box draw_ball(
 	.X_pix(X_pix),
@@ -77,13 +100,21 @@ make_box draw_ball(
 	.box(ball)
 );
 
+logic [19:0] i = 0;
 
-always @(posedge CLOCK_50)
+always_ff @(posedge CLOCK_50)
 	begin
 		if(player_1_paddle) pixel_color <= 12'b1111_1111_1111;
 		else if(player_2_paddle) pixel_color <= 12'b1111_1111_1111;
 		else if (ball) pixel_color <= 12'b1111_1111_1111;
 		else pixel_color <= 12'b0000_0000_0000;
+		
+		i = i + 1;
+		
+		if (i == 307200) begin
+			i <= 0;
+			ball_y_location_logic = ball_y_location_logic + 1;
+		end
 	end
 	
 		//Pass pins and current pixel values to display driver
--- modules/make_box.sv
+++ modules/make_box.sv
@@ -1,16 +1,19 @@
 module make_box(
-	input [9:0] X_pix,
-	input [9:0] Y_pix,
-	input [9:0] box_width,
-	input [9:0] box_height,
-	input [9:0] box_x_location,
-	input [9:0] box_y_location,
-	input pixel_clk,
-	output reg box
+    input logic [9:0] X_pix,
+    input logic [9:0] Y_pix,
+    input logic [9:0] box_width,
+    input logic [9:0] box_height,
+    input logic [9:0] box_x_location,
+    input logic [9:0] box_y_location,
+    input logic pixel_clk,
+    output logic box
 );
-	always @(posedge pixel_clk) begin
-		if((X_pix>box_x_location) && (X_pix<(box_x_location + box_width)) && (Y_pix>box_y_location) && (Y_pix<(box_y_location + box_height))) 
-			box = 1;
-		else	box = 0;
-	end
+
+always_ff @(posedge pixel_clk)
+    if ((X_pix > box_x_location) && (X_pix < (box_x_location + box_width)) &&
+        (Y_pix > box_y_location) && (Y_pix < (box_y_location + box_height)))
+        box <= 1;
+    else
+        box <= 0;
+
 endmodule
